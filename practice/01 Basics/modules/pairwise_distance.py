9fdc8374e611c07e5b5b0d203704183daf814417
NoKorun <45893644+NoKorun@users.noreply.github.com>
2024-09-24 02:26:44 +0500

    Update pairwise_distance.py, added calculate and choose distance func

--- practice/01 Basics/modules/pairwise_distance.py
+++ practice/01 Basics/modules/pairwise_distance.py
@@ -2,6 +2,8 @@ import numpy as np
 
 from modules.metrics import ED_distance, norm_ED_distance, DTW_distance
 from modules.utils import z_normalize
+from numpy.lib.arraysetops import ediff1d
+from sktime.distances import dtw_distance
 
 
 class PairwiseDistance:
@@ -50,6 +52,10 @@ class PairwiseDistance:
         dist_func = None
 
         # INSERT YOUR CODE
+        if self.metric == "euclidean":
+          dist_func = ED_distance
+        else:
+          dist_func = dtw_distance
 
         return dist_func
 
@@ -70,5 +76,17 @@ class PairwiseDistance:
         matrix_values = np.zeros(shape=matrix_shape)
         
         # INSERT YOUR CODE
+        if self.is_normalize and self.metric != 'euclidean':
+          input_data = z_normalize(input_data)
+        n = input_data.shape[0]
+
+        dist_func = self._choose_distance()
+        for i in range(n):
+          for j in range(i,n):
+            if i == j:
+              matrix_values[i,j] = 0
+              continue
+            matrix_values[i,j] = dist_func(input_data[i],input_data[j])
+            matrix_values[j,i] = matrix_values[i,j]
 
         return matrix_values
